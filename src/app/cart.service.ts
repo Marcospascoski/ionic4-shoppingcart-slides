7cf60450dcb4ed00263475ab1db9fa9e81d36906
vcjpierre <vcjpierre@gmail.com>
2018-12-17 22:25:51 -0400

    Added images and update UI

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,25 +8,25 @@ export class CartService {
       category: 'Pizza',
       expanded: true,
       products: [
-        { id: 0, name: 'Salami', price: '8' },
-        { id: 1, name: 'Classic', price: '5' },
-        { id: 2, name: 'Tuna', price: '9' },
-        { id: 3, name: 'Hawai', price: '7' }
+        { id: 0, name: 'Salami', price: '8', imageUrl: '/assets/images/salami.jpg' },
+        { id: 1, name: 'Classic', price: '5', imageUrl: '/assets/images/classic.jpg' },
+        { id: 2, name: 'Tuna', price: '9', imageUrl: '/assets/images/tuna.jpg' },
+        { id: 3, name: 'Hawai', price: '7', imageUrl: '/assets/images/hawai.jpg' }
       ]
     },
     {
       category: 'Pasta',
       products: [
-        { id: 4, name: 'Mac & Cheese', price: '8' },
-        { id: 5, name: 'Bolognese', price: '6' }
+        { id: 4, name: 'Macaroni ', price: '8', imageUrl: '/assets/images/mac.jpg' },
+        { id: 5, name: 'Bolognese', price: '6', imageUrl: '/assets/images/bolagnese.jpg' }
       ]
     },
     {
       category: 'Salad',
       products: [
-        { id: 6, name: 'Ham & Egg', price: '8' },
-        { id: 7, name: 'Basic', price: '5' },
-        { id: 8, name: 'Ceaser', price: '9' }
+        { id: 6, name: 'Ham', price: '8', imageUrl: '/assets/images/ham.jpg'  },
+        { id: 7, name: 'Basic', price: '5', imageUrl: '/assets/images/basic.jpg'  },
+        { id: 8, name: 'Ceaser', price: '9', imageUrl: '/assets/images/ceaser.jpg'  }
       ]
     }
   ];
